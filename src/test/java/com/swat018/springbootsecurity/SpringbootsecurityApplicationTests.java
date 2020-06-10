a08532d9b78c8d874a72b75b5a6a912e9157d68d
jinwoo park <swat018@gmail.com>
2020-06-10 23:24:59 +0900

    Initial commit

---
+++ src/test/java/com/swat018/springbootsecurity/SpringbootsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swat018.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
